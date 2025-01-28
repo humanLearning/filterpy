1bfefe7a0c531b455da225fdba8e12da74d48403
ankitvyn <ankit.parmar@humanlearning.com>
2025-01-28 14:53:42 +0530

    add version, remove filterpy import

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version=filterpy.__version__,
+    version="1.0.0",
 
     description='Kalman filtering and optimal estimation library',
     long_description=long_description,
